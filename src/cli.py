cb8f2952039eda5193005de7c8c9bfc574f259ba
Colin <cjfaught99@gmail.com>
2020-12-21 15:26:04 -0500

    add to stock news feature extended to cli

--- src/cli.py
+++ src/cli.py
@@ -1,11 +1,14 @@
 import click
+import store_news
 
 @click.command()
 #@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--stock', prompt='Add stock:', help='Enter the stock symbol to monitor for news updates.')
+@click.option('--stock', prompt='Add stock', help='Enter the stock symbol to monitor for news updates.')
 def add_stock(stock):
     """Simple program that greets NAME for a total of COUNT times."""
     print(f'adding: {stock}')
+    store_news.add_stock_to_news(stock)
+
 
 if __name__ == '__main__':
     add_stock()
