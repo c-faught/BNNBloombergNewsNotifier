e07962a22fff3eb1476e83e0480b6ed60543ce61
Colin <cjfaught99@gmail.com>
2020-12-21 20:36:23 -0500

    removed config from gitignore

--- src/cli.py
+++ src/cli.py
@@ -1,13 +1,23 @@
 import click
+import store_news
 
 @click.command()
-#@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--stock', prompt='Add stock:', help='Enter the stock symbol to monitor for news updates.')
-def add_stock(stock):
-    """Simple program that greets NAME for a total of COUNT times."""
-    print(f'adding: {stock}')
+
+@click.option('--add', prompt='Add stock', help='Enter the stock symbol to monitor for news updates.')
+def add_stock(add):
+    print(f'adding: {add}')
+    store_news.add_stock_to_news(add)
+
+@click.command()
+
+@click.option('--remove', prompt='Remove stock', help='Enter the stock symbol to remove from news updates.')
+def remove_stock(remove):
+    print(f'removing: {remove}')
+    store_news.remove_stock_to_news(remove)
 
 if __name__ == '__main__':
-    add_stock()
+    remove_stock()
+
+
 
 #python cli.py --stock=ESI:CT
